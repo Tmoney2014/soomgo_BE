e50a2e0865f8a654ced1b94597a2fadad64421f5
yougeun6021 <dbrms1a1a@naver.com>
2022-07-29 14:44:42 +0900

    first commit

---
+++ src/test/java/com/clone/soomgo/SoomgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clone.soomgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoomgoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
