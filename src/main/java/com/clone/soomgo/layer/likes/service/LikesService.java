5001e634105e24b96f3138818c7a8bb6695f666a
Tmoney2014 <107834618+Tmoney2014@users.noreply.github.com>
2022-07-30 10:32:09 +0900

    likes 구현 테스트 완료

--- src/main/java/com/clone/soomgo/layer/likes/service/LikesService.java
+++ src/main/java/com/clone/soomgo/layer/likes/service/LikesService.java
@@ -24,8 +24,8 @@ public class LikesService {
 
     public ResponseEntity<?> postLikes(Long userId, Long postId) {
 
-        User user = userRepository.findByuserId(userId);
-        Post post = postRepository.findBypostId(postId);
+        User user = userRepository.findById(userId).orElseThrow(() -> new NullPointerException("유저가 없습니다"));
+        Post post = postRepository.findById(postId).get();
         Likes like = new Likes(user, post);
         likesRepository.save(like);
 
