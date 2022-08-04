9243dbd410f647a8df167793280fda5e44c829fd
yougeun6021 <dbrms1a1a@naver.com>
2022-08-04 13:03:31 +0900

    post 통합테스트 추가

--- src/test/java/com/clone/soomgo/IntegrationTest/PostIntegrationTest.java
+++ src/test/java/com/clone/soomgo/IntegrationTest/PostIntegrationTest.java
@@ -6,6 +6,7 @@ import com.clone.soomgo.config.security.jwt.JwtTokenUtils;
 import com.clone.soomgo.layer.ImgUrl.dto.ImgUrlDto;
 import com.clone.soomgo.layer.post.dto.PostResponseDto;
 import com.clone.soomgo.layer.post.dto.TagDto;
+import com.clone.soomgo.layer.post.dto.ViewCountPostResponseDto;
 import com.clone.soomgo.layer.post.model.SubjectEnum;
 import com.clone.soomgo.layer.post.repository.PostRepository;
 import com.clone.soomgo.layer.user.model.User;
@@ -113,7 +114,7 @@ public class PostIntegrationTest {
     @Test
     @Order(2)
     @DisplayName("포스트 전체 들고오기")
-    void test2() throws JsonProcessingException{
+    void test2(){
 
         String subject = "ALL";
         int page =0;
@@ -124,7 +125,6 @@ public class PostIntegrationTest {
 
         );
 
-
         assertEquals(HttpStatus.OK,response.getStatusCode());
 
     }
@@ -132,7 +132,7 @@ public class PostIntegrationTest {
     @Test
     @Order(3)
     @DisplayName("포스트 상세페이지 들고오기")
-    void test3() throws  JsonProcessingException{
+    void test3(){
         long postId = 101L;
 
         HttpEntity<String> request = new HttpEntity<>(headers);
@@ -158,7 +158,7 @@ public class PostIntegrationTest {
     @Test
     @Order(4)
     @DisplayName("존재하지 않는 포스트 상세페이지")
-    void test4() throws  JsonProcessingException{
+    void test4(){
         long postId = 200L;
 
         HttpEntity<String> request = new HttpEntity<>(headers);
@@ -171,8 +171,177 @@ public class PostIntegrationTest {
         );
 
         assertEquals(HttpStatus.BAD_REQUEST,response.getStatusCode());
+    }
+
+    @Test
+    @Order(5)
+    @DisplayName("포스트 업데이트 하기")
+    void test5() throws JsonProcessingException{
+        List<TagDto> tagDtoList = new ArrayList<>();
+
+        List<ImgUrlDto> imgUrlDtoList = new ArrayList<>();
+
+        TagDto tagDto = new TagDto("태그수정");
+
+        ImgUrlDto imgUrlDto = new ImgUrlDto("URL수정");
+
+        tagDtoList.add(tagDto);
+        imgUrlDtoList.add(imgUrlDto);
+
+        PostRequestDto postRequestDto = PostRequestDto.builder()
+                .content("내용수정")
+                .title("제목수정")
+                .subject(SubjectEnum.QNA)
+                .imgUrlList(imgUrlDtoList)
+                .tagList(tagDtoList)
+                .build();
+
+        String requestBody = mapper.writeValueAsString(postRequestDto);
+
+        HttpEntity<String> request = new HttpEntity<>(requestBody, headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(
+                "/api/posts/101",
+                HttpMethod.PUT,
+                request,
+                String.class
+        );
+
+        assertEquals(HttpStatus.OK,response.getStatusCode());
 
     }
+
+    @Test
+    @Order(6)
+    @DisplayName("포스트 업데이트에서 중복된 태그가 존재하는 경우")
+    void test6() throws JsonProcessingException{
+        List<TagDto> tagDtoList = new ArrayList<>();
+
+        List<ImgUrlDto> imgUrlDtoList = new ArrayList<>();
+
+        TagDto tagDto = new TagDto("태그수정");
+        TagDto tagDto2 = new TagDto("태그수정");
+
+        ImgUrlDto imgUrlDto = new ImgUrlDto("URL수정");
+
+        tagDtoList.add(tagDto);
+        tagDtoList.add(tagDto2);
+        imgUrlDtoList.add(imgUrlDto);
+
+        PostRequestDto postRequestDto = PostRequestDto.builder()
+                .content("내용수정")
+                .title("제목수정")
+                .subject(SubjectEnum.QNA)
+                .imgUrlList(imgUrlDtoList)
+                .tagList(tagDtoList)
+                .build();
+
+        String requestBody = mapper.writeValueAsString(postRequestDto);
+
+        HttpEntity<String> request = new HttpEntity<>(requestBody, headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(
+                "/api/posts/101",
+                HttpMethod.PUT,
+                request,
+                String.class
+        );
+
+        assertEquals(HttpStatus.BAD_REQUEST,response.getStatusCode());
+        assertEquals("중복된 태그가 존재합니다",response.getBody());
+
+    }
+
+    @Test
+    @Order(7)
+    @DisplayName("포스트 업데이트에서 태그에 , 가 존재하는 경우")
+    void test7() throws JsonProcessingException{
+        List<TagDto> tagDtoList = new ArrayList<>();
+
+        List<ImgUrlDto> imgUrlDtoList = new ArrayList<>();
+
+        TagDto tagDto = new TagDto("태그,수정");
+
+        ImgUrlDto imgUrlDto = new ImgUrlDto("URL수정");
+
+        tagDtoList.add(tagDto);
+
+        imgUrlDtoList.add(imgUrlDto);
+
+        PostRequestDto postRequestDto = PostRequestDto.builder()
+                .content("내용수정")
+                .title("제목수정")
+                .subject(SubjectEnum.QNA)
+                .imgUrlList(imgUrlDtoList)
+                .tagList(tagDtoList)
+                .build();
+
+        String requestBody = mapper.writeValueAsString(postRequestDto);
+
+        HttpEntity<String> request = new HttpEntity<>(requestBody, headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(
+                "/api/posts/101",
+                HttpMethod.PUT,
+                request,
+                String.class
+        );
+
+        assertEquals(HttpStatus.BAD_REQUEST,response.getStatusCode());
+        assertEquals(" ,이 들어간 태그는 쓸 수 없습니다.",response.getBody());
+
+    }
+
+    @Test
+    @Order(9)
+    @DisplayName("포스트 삭제하기")
+    void test8(){
+        long postId = 101L;
+
+        HttpEntity<String> request = new HttpEntity<>(headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(
+                "/api/posts/"+postId,
+                HttpMethod.DELETE,
+                request,
+                String.class
+        );
+
+        assertEquals(HttpStatus.OK,response.getStatusCode());
+        assertEquals("정상적으로 삭제 되었습니다",response.getBody());
+    }
+
+    @Test
+    @Order(8)
+    @DisplayName("포스트 조회순으로 정렬하기")
+    void test9(){
+
+
+        ResponseEntity<ViewCountPostResponseDto> response = restTemplate.getForEntity(
+                "/api/posts/viewcount",
+                ViewCountPostResponseDto.class
+
+        );
+
+        ViewCountPostResponseDto result = response.getBody();
+
+
+
+        assertEquals(HttpStatus.OK,response.getStatusCode());
+        assertEquals(4, Objects.requireNonNull(result).getPostList().size());
+        assertEquals(101,result.getPostList().get(0).getPostId());
+
+    }
+
+
+
+
+
+
+
+
+
+
 
     @Setter
     @Getter
