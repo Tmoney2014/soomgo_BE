c684ccfbaf5c246f834883ec95c53541baa6ada0
MoonDoorKing <rizingsniper@kakao.com>
2022-08-04 14:43:30 +0900

    UPDATE : CODE MODIFY

--- src/main/java/com/clone/soomgo/layer/comment/repository/CommentRepository.java
+++ src/main/java/com/clone/soomgo/layer/comment/repository/CommentRepository.java
@@ -11,5 +11,5 @@ import java.util.List;
 import java.util.Optional;
 
 public interface CommentRepository extends JpaRepository<Comment,Long> {
-    List<Comment> findByUserAndPost(User user, Post post);
+    List<Comment> findAllByPost(Post post);
 }
